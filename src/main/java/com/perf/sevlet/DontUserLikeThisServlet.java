5cff1c155ff866f344ba083841d5a1d53f65fd2a
IMWoo94 <leemeawoo94@gmail.com>
2023-09-19 17:54:23 +0900

    java servlet create 2023.09.19

---
+++ src/main/java/com/perf/sevlet/DontUserLikeThisServlet.java
@@ -0,0 +1,23 @@
+package com.perf.sevlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DontUserLikeThisServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	String successFlag = "N";
+
+	public DontUserLikeThisServlet() {
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		successFlag = request.getParameter("successFlag");
+	}
+}
